bbc1a7471cba1e9666d172c6e0f8a5740ceae852
Ballista Milsim <ballista.milsim@gmail.com>
2017-04-17 16:49:05 +0200

    tb_rhs_zavod_311.chernarus mission added

---
+++ briefing_red.hpp
@@ -0,0 +1,12 @@
+// Provide side specific mission briefing here. Text can use structured text syntax. Use <br /> tag for line breaks, plus all other Arma tags.
+<titleFont>Gerneral Reminders:</titleFont><br />
+<*> <green>Squad leaders</green> - take the SL kit and deploy <RP> Rally Points<br />
+<*> Help out your team by deploying <FO> Forward Outposts<br />
+<br />
+
+<titleFont>Deployment Options:</titleFont><br />
+<boxes> Forward Base<br />
+<FO> Forward Outpost<br />
+<RP> Rally Point<br />
+<br />
+
